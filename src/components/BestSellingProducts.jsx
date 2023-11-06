a22d3bffefbcf99ff81f8b064c81edbec2a41c4d
Palash Nath <palashnath880@gmail.com>
2023-11-06 23:13:39 +0600

    Swiper Slider Added

--- src/components/BestSellingProducts.jsx
+++ src/components/BestSellingProducts.jsx
@@ -1,11 +1,88 @@
 import { useState } from "react";
+import { SwiperSlide, Swiper } from 'swiper/react'
+import 'swiper/css';
+
+//assets
+import bedImg1 from '../assets/product/bed-1.webp';
+import bedImg2 from '../assets/product/bed-2.webp';
+import bedImg3 from '../assets/product/bed-3.webp';
+import bedImg4 from '../assets/product/bed-4.webp';
+import sofaImg1 from '../assets/product/sofa-1.webp';
+import sofaImg2 from '../assets/product/sofa-2.webp';
+import sofaImg3 from '../assets/product/sofa-3.webp';
+import sofaImg4 from '../assets/product/sofa-4.webp';
+import chairImg1 from '../assets/product/chair-1.webp';
+import chairImg2 from '../assets/product/chair-2.webp';
+import chairImg3 from '../assets/product/chair-3.webp';
+import chairImg4 from '../assets/product/chair-4.webp';
+import lampImg1 from '../assets/product/lamp-1.webp';
+import lampImg2 from '../assets/product/lamp-2.webp';
+import lampImg3 from '../assets/product/lamp-3.webp';
+import lampImg4 from '../assets/product/lamp-4.webp';
 
 const BestSellingProducts = () => {
 
     // states
     const [activeTab, setActiveTab] = useState(0);
 
-    const tabs = ['Chair', 'Beds', 'Sofa', 'Lamp'];
+    const tabs = ['Chair', 'Bed', 'Sofa', 'Lamp'];
+
+    const data = {
+        chair: [
+            {
+                title: "Eames Lounge Chair",
+                price: 5000,
+                img: chairImg1
+            },
+            {
+                title: "Aeron Chair",
+                price: 1000,
+                img: chairImg2
+            },
+            {
+                title: "Wishbone Chair",
+                price: 500,
+                img: chairImg3
+            },
+            {
+                title: "Tolix Chair",
+                price: 200,
+                img: chairImg1
+            },
+            {
+                title: "IKEA Poäng Chair",
+                price: 100,
+                img: chairImg4
+            }
+        ],
+        bed: [
+            {
+                title: "Eames Lounge Chair",
+                price: 5000,
+                img: chairImg1
+            },
+            {
+                title: "Aeron Chair",
+                price: 1000,
+                img: chairImg2
+            },
+            {
+                title: "Wishbone Chair",
+                price: 500,
+                img: chairImg3
+            },
+            {
+                title: "Tolix Chair",
+                price: 200,
+                img: chairImg1
+            },
+            {
+                title: "IKEA Poäng Chair",
+                price: 100,
+                img: chairImg4
+            }
+        ],
+    };
 
     return (
         <section>
@@ -17,6 +94,55 @@ const BestSellingProducts = () => {
                             {tab}
                         </button>)}
                     </div>
+                </div>
+                <div className="mt-10">
+                    <Swiper
+                        speed={1000}
+                        slidesPerView={4}
+                        spaceBetween={10}
+                        loop={true}
+                    >
+                        {activeTab === 0 && [chairImg1, chairImg2, chairImg3, chairImg4].map((img, index) => <SwiperSlide key={index}>
+                            <div className="">
+                                <div>
+                                    <img src={img} alt={img} className="" />
+                                </div>
+                                <div className="flex flex-col">
+                                    <h3 className="">{index + 1}</h3>
+                                </div>
+                            </div>
+                        </SwiperSlide>)}
+                        {activeTab === 1 && [bedImg1, bedImg2, bedImg3, bedImg4].map((img, index) => <SwiperSlide key={index}>
+                            <div className="">
+                                <div>
+                                    <img src={img} alt={img} className="" />
+                                </div>
+                                <div className="flex flex-col">
+                                    <h3 className="">{index + 1}</h3>
+                                </div>
+                            </div>
+                        </SwiperSlide>)}
+                        {activeTab === 2 && [sofaImg1, sofaImg2, sofaImg3, sofaImg4].map((img, index) => <SwiperSlide key={index}>
+                            <div className="">
+                                <div>
+                                    <img src={img} alt={img} className="" />
+                                </div>
+                                <div className="flex flex-col">
+                                    <h3 className="">{index + 1}</h3>
+                                </div>
+                            </div>
+                        </SwiperSlide>)}
+                        {activeTab === 3 && [lampImg1, lampImg2, lampImg3, lampImg4].map((img, index) => <SwiperSlide key={index}>
+                            <div className="">
+                                <div>
+                                    <img src={img} alt={img} className="" />
+                                </div>
+                                <div className="flex flex-col">
+                                    <h3 className="">{index + 1}</h3>
+                                </div>
+                            </div>
+                        </SwiperSlide>)}
+                    </Swiper>
                 </div>
             </div>
         </section>
--- src/components/HeroArea.jsx
+++ src/components/HeroArea.jsx
@@ -1,14 +1,65 @@
-import bgImg from '../assets/hero-bg.webp';
 import { BiSearch } from 'react-icons/bi';
 
+// assets
+import img1 from '../assets/hero-slide-1.jpg';
+import img2 from '../assets/hero-slide-2.jpg';
+import { useEffect, useRef } from 'react';
+
 const HeroArea = () => {
+
+    // states
+    const ref = useRef();
+
+    // slider images array
+    const images = [img1, img2,];
+
+    useEffect(() => {
+        const wrapper = ref.current;
+        const slides = wrapper.children;
+        let activeIndex = 0;
+        let interval;
+
+        const __change_slide = (index) => {
+            //  add opacity
+            for (let slide of slides) {
+                const slide_index = parseInt(slide.getAttribute('data-index'));
+                slide.classList.remove('opacity-100');
+                slide.classList.add('opacity-0');
+
+                if (slide_index === index) {
+                    slide.classList.add('opacity-100');
+                }
+            }
+        }
+
+        __change_slide(activeIndex);
+
+        interval = setInterval(() => {
+
+            if (activeIndex === slides.length - 1) {
+                activeIndex = 0;
+            } else {
+                activeIndex++;
+            }
+
+            __change_slide(activeIndex);
+        }, 5000);
+
+        return () => clearInterval(interval);
+
+    }, []);
+
     return (
         <div className="w-full h-screen relative">
-            <div className='w-full h-full'>
-                <img src={bgImg} alt='Hero BG' className='w-full h-full object-cover' />
-            </div>
 
-            <div className='mx-auto px-5 absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full md:w-[700px]'>
+            <div className='w-full h-full relative' ref={ref}>
+                {images.map((image, index) => <div key={index} data-index={index} className='absolute top-0 left-0 w-full h-full slide opacity-0 duration-1000'>
+                    <img src={image} alt='Hero BG' className='w-full h-full object-cover' draggable={false} />
+                </div>)}
+            </div>
+            <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-30' />
+
+            <div className='mx-auto px-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full md:w-[700px]'>
                 <div className='flex flex-col items-center gap-7'>
                     <h1 className='text-6xl font-Gilroy-Bold text-center text-white leading-tight'>
                         Make your interior more minimalistic & modern
