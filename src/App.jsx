bb7c726ce06fed8963e41bc314b28012ebefbac2
Palash Nath <palashnath880@gmail.com>
2023-11-10 11:34:02 +0600

    Materials Area Update

--- src/App.jsx
+++ src/App.jsx
@@ -57,11 +57,11 @@ const App = () => {
       {/* materials */}
       <section className="py-20 bg-white">
         <div className="container mx-auto px-5 max-sm:px-3">
-          <div className="flex items-center gap-20">
-            <div className="w-1/2">
+          <div className="flex items-center max-md:flex-col-reverse max-lg:gap-10 gap-20">
+            <div className="max-md:w-full w-1/2">
               <div className="flex flex-col gap-y-4 lg:w-[450px]">
                 <span className="text-sm font-Gilroy-Medium text-secondary uppercase tracking-widest">Materials</span>
-                <h1 className="font-Gilroy-Bold text-4xl leading-tight text-[#1E1E1E]">Very serious <br /> materials for making <br /> furniture</h1>
+                <h1 className="font-Gilroy-Bold max-sm:text-3xl text-4xl leading-tight text-[#1E1E1E]">Very serious <br className="" /> materials for making <br className="max-lg:hidden" /> furniture</h1>
                 <p className="font-Gilroy-Light text-sm text-[#1E1E1E] leading-relaxed">
                   Because panto was very serious about designing furniture for our environment, using a very expensive and famous capital but at a relatively low price
                 </p>
@@ -71,7 +71,7 @@ const App = () => {
                 </button>
               </div>
             </div>
-            <div className="w-1/2">
+            <div className="max-md:w-full w-1/2">
               <div className="w-full aspect-square grid grid-cols-3 gap-4">
                 <div className="col-span-1 flex flex-col gap-4">
                   <div className="flex-1 rounded-lg shadow-lg overflow-hidden">
